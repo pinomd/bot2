4317a0947d5ad05fc15694ffbcc651ac8243f3df
pinomd <108236810+pinomd@users.noreply.github.com>
2022-06-28 10:21:59 +0700

    Update ainetoxic.js

--- plugins/ainetoxic.js
+++ plugins/ainetoxic.js
@@ -1,11 +1,9 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let gaboleh = fs.readFileSync('./mp3/PTT-20220220-WA0414.opus')
-let ihhomkasar = fs.readFileSync('./mp3/WhatsApp-Audio-2021-06-16-at-14.35.33.opus')
+let ihhomkasar = fs.readFileSync('./mp3/PTT-20220628-WA0394.opus')
 conn.sendFile(m.chat, ihhomkasar, '', '', m, true)
 setTimeout(() => {
-conn.sendFile(m.chat, gaboleh, '', '', m, true)
 }, 1000)
 //conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
